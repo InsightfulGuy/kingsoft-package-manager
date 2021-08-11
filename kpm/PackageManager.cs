79328de7e23de8448774c251c6f80fc819c73355
TimKral5 <TimKral5@outlook.com>
2021-08-11 15:19:36 +0200

    Projektdateien hinzufügen.

---
+++ kpm/PackageManager.cs
@@ -0,0 +1,224 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+using System.Net;
+using System.IO.Compression;
+using Spectre.Console;
+using System.Reflection;
+
+namespace kpm
+{
+	public class PackFileAction
+	{
+		public string Type { get; set; }
+		public string Uri { get; set; }
+		public string Path { get; set; }
+		public string Path2 { get; set; }
+	}
+
+	public class PackageFile
+	{
+		public PackFileAction[] Actions { get; set; }
+	}
+
+	public class PackageRegistryEntree
+	{
+		public string Id { get; set; }
+		public string Author { get; set; }
+		public string Description { get; set; }
+		public string PackageFileUri { get; set; }
+	}
+
+	public class PackageRegistryFile
+	{
+		public string ServerName { get; set; }
+		public PackageRegistryEntree[] Entrees { get; set; }
+	}
+
+	public class PackageManager
+	{
+		private static Dictionary<string, string> Nodes { get; set; }
+		private static WebClient _WebClient { get; set; }
+
+		public static void InitPM()
+		{
+			string confFilePath = $"{Program.AppPath}/packRegUris.conf";
+			Nodes = new Dictionary<string, string>();
+			string mainNodeUri = "http://kingsoft.dyndns-home.com:19138/kpm/Node.json";
+			if (!File.Exists(confFilePath)) File.Create(confFilePath).Close();
+			if (string.IsNullOrEmpty(File.ReadAllText(confFilePath))) File.WriteAllText(confFilePath, $"main:{mainNodeUri}");
+
+			string[] configFileContents = File.ReadAllLines(confFilePath);
+
+			for (int i = 0; i < configFileContents.Length; i++)
+			{
+				string line = configFileContents[i];
+				if (line.StartsWith("main:"))
+				{
+					mainNodeUri = line.Remove(0, "main:".Length);
+					Nodes.Add("main", mainNodeUri);
+				}
+				else if (!line.StartsWith("#")) Nodes.Add(line.Split(':')[0], line.Remove(0, $"{line.Split(':')[0]}:".Length));
+			}
+		}
+
+		public static void InstallPackageById(string id)
+		{
+			bool installPackage(PackageRegistryEntree entree)
+			{
+				ExecutePackageFile(_WebClient.DownloadString(entree.PackageFileUri));
+				return true;
+			}
+
+			ListPackages(installPackage, id, "id");
+		}
+
+		public static void SearchPackages(Func<PackageRegistryEntree, bool> printResult, string query, string qType)
+		{
+			ListPackages(printResult, query, qType, false);
+		}
+
+		private static void ListPackages(Func<PackageRegistryEntree, bool> entreeFunction, string query, string qType, bool justOneResult=true)
+		{
+			bool resFound = false;
+			foreach (KeyValuePair<string, string> node in Nodes)
+			{
+				_WebClient = new WebClient();
+
+				PackageRegistryFile registryFile = 
+					JsonConvert.DeserializeObject<PackageRegistryFile>(_WebClient.DownloadString(node.Value));
+
+
+				for (int i = 0; i < registryFile.Entrees.Length; i++)
+				{
+					PackageRegistryEntree entree = registryFile.Entrees[i];
+
+					switch (qType)
+					{
+						case "id":
+							if (justOneResult)
+							{
+								if (entree.Id == query)
+								{
+									entreeFunction(entree);
+									return;
+								}
+							}
+							else
+							{
+								if (entree.Id.Contains(query))
+								{
+									entreeFunction(entree);
+									resFound = true;
+								}
+							}
+							
+							break;
+						default:
+							return;
+					}
+				}
+			}
+			if(!resFound) entreeFunction(new PackageRegistryEntree() { Id="<ERROR>" });
+		}
+
+		public static void ExecutePackageFile(string packageFileContent)
+		{
+			string DecodeFilePath(string inPath)
+			{
+				string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+				desktopPath = desktopPath.Replace("\\", "/");
+				string commonStartMenuPath = Environment.GetFolderPath(Environment.SpecialFolder.CommonStartMenu);
+				commonStartMenuPath = commonStartMenuPath.Replace("\\", "/");
+
+				string programsPath = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles);
+				programsPath = programsPath.Replace("\\", "/");
+
+				string programs86Path = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86);
+				programs86Path = programs86Path.Replace("\\", "/");
+
+				if (inPath.Contains("{Current}")) inPath = inPath.Replace(
+					(inPath.Contains("{Current}/") && Program.CurrentPath == "") 
+					? "{Current}/" : "{Current}", Program.CurrentPath != "" ? Program.CurrentPath : "");
+
+				if (inPath.Contains("{Desktop}"))
+					inPath = inPath.Replace("{Desktop}", desktopPath);
+
+				if (inPath.Contains("{StartMenu}"))
+					inPath = inPath.Replace("{StartMenu}", commonStartMenuPath);
+
+				if (inPath.Contains("{Programs}"))
+					inPath = inPath.Replace("{Programs}", programsPath);
+
+				if (inPath.Contains("{Programs86}"))
+					inPath = inPath.Replace("{Programs86}", programs86Path);
+
+				return inPath;
+			}
+
+			PackageFile packageFile = JsonConvert.DeserializeObject<PackageFile>(packageFileContent);
+			AnsiConsole.Render(new Text("\n\n"));
+			AnsiConsole.Status().Start("installing package...", ctx =>
+			{
+				ctx.Spinner = Spinner.Known.Aesthetic;
+
+
+				for (int i = 0; i < packageFile.Actions.Length; i++)
+				{
+					PackFileAction action = packageFile.Actions[i];
+
+					if (action.Path != null) action.Path = DecodeFilePath(action.Path);
+					if (action.Path2 != null) action.Path2 = DecodeFilePath(action.Path2);
+
+					switch (action.Type)
+					{
+						case "download":
+
+							if (!File.Exists(action.Path)) File.Create(action.Path).Close();
+							_WebClient.DownloadFile(new Uri(action.Uri), action.Path);
+							AnsiConsole.MarkupLine($"[grey]LOG:[/] downloaded file from [u blue link={action.Uri}]uri[/]");
+							ctx.Refresh();
+							break;
+						case "mkdir":
+							Directory.CreateDirectory(action.Path);
+							AnsiConsole.MarkupLine($"[grey]LOG:[/] made Directory \"{action.Path}\"");
+							ctx.Refresh();
+							break;
+						case "mkfile":
+							if (!File.Exists(action.Path)) File.Create(action.Path).Close();
+							else File.WriteAllText(action.Path, "");
+							AnsiConsole.MarkupLine($"[grey]LOG:[/] made File \"{action.Path}\"");
+							ctx.Refresh();
+							break;
+						case "movdir":
+							Directory.Move(action.Path, action.Path2);
+							AnsiConsole.MarkupLine($"[grey]LOG:[/] moved directory from \"{action.Path}\" to \"{action.Path2}\"");
+							break;
+						case "movfile":
+							File.Move(action.Path, action.Path2);
+							AnsiConsole.MarkupLine($"[grey]LOG:[/] moved file from \"{action.Path}\" to \"{action.Path2}\"");
+							break;
+						case "remdir":
+							Directory.Delete(action.Path);
+							break;
+						case "remfile":
+							File.Delete(action.Path);
+							break;
+						case "unzip":
+							if (!Directory.Exists(action.Path2)) Directory.CreateDirectory(action.Path2);
+							ZipFile.ExtractToDirectory(action.Path, action.Path2);
+							AnsiConsole.MarkupLine($"extracted Directory \"{action.Path}\" to \"{action.Path2}\"");
+							break;
+						default:
+							break;
+					}
+				}
+			});
+		}
+	}
+}
---
+++ kpm/Program.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Spectre.Console;
+using Spectre.Console.Rendering;
+using Newtonsoft.Json;
+using UtilityLib.Conversion;
+using System.IO;
+using System.Windows.Forms;
+using System.Windows;
+
+namespace kpm
+{
+	class Program
+	{
+		public static string CurrentPath { get; set; }
+		public static string AppPath { get; set; }
+		public static bool EnableGui { get; set; }
+
+		private static Table RenderCommandTable(string title, string[] columns, string[][] rows)
+		{
+			Table commands = new Table();
+			commands.AddColumns(columns);
+			for (int i = 0; i < rows.Length; i++)
+			{
+				string[] row = rows[i];
+				commands.AddRow(row);
+			}
+			commands.Centered().RoundedBorder().Title = new TableTitle(title);
+			return commands;
+		}
+
+		private static void RenderCommandTable(Table commandTable)
+		{
+			AnsiConsole.Render(new Text("\n\n"));
+			AnsiConsole.Render(commandTable);
+			return;
+		}
+
+		#region installCommand
+		private static readonly Table installCommand =
+			RenderCommandTable(
+				"install",
+				new string[3]
+				{
+					"Parameter ID",
+					"Usage",
+					"description"
+				},
+				new string[2][]
+				{
+					new string[3]
+					{
+						"-id",
+						"kpm install -id <unique id of the package>",
+						"installs the package with the entered id"
+					},
+					new string[3]
+					{
+						"-p",
+						"kpm install -p <installation path>",
+						"installs the package in the entered directory"
+					}
+				}
+				);
+		#endregion
+
+		#region updateCommand
+		private static readonly Table updateCommand =
+			RenderCommandTable(
+				"update",
+				new string[3]
+				{
+					"Parameter ID",
+					"Usage",
+					"description"
+				},
+				new string[1][]
+				{
+					new string[3] { "<null>", "kpm update", "updates all packages if updates are available" }
+				}
+				);
+		#endregion
+
+		#region searchCommand
+		private static readonly Table searchCommand =
+			RenderCommandTable(
+				"search",
+				new string[3]
+				{
+					"Parameter ID",
+					"Usage",
+					"description"
+				},
+				new string[1][]
+				{
+					new string[3]
+					{
+						"-id",
+						"kpm search -id <search query>",
+						"lists all packages which id contains the entered query"
+					}
+				}
+				);
+		#endregion
+
+		static void Main(string[] args)
+		{
+			EnableGui = false;
+
+			string AppName = "kpm.exe";
+			AppPath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+			AppPath = AppPath.Remove(AppPath.Length - 1 - AppName.Length);
+			PackageManager.InitPM();
+			var starwarsFont = FigletFont.Load($"{AppPath}/block.flf");
+
+
+			void RenderLogo()
+			{
+				AnsiConsole.Render(
+					new FigletText(starwarsFont, "KPM")
+					.Centered()
+					.Color(Color.Green)
+				);
+				AnsiConsole.Render(
+					new Markup("[green u link=http://tim.krals.ch]kingsoft products[/]").Centered()
+				);
+			}
+
+			RenderLogo();
+
+			switch (args.Length)
+			{
+				case 0:
+					RenderCommandTable(installCommand);
+					RenderCommandTable(updateCommand);
+					RenderCommandTable(searchCommand);
+					break;
+				case 1:
+					switch (args[0])
+					{
+						case "-gui":
+							EnableGui = true;
+							kpmHelpForm form = new kpmHelpForm();
+							Application.Run(form);
+							break;
+						default:
+							Console.WriteLine(CurrentPath);
+							PackageManager.ExecutePackageFile(File.ReadAllText(args[0]));
+							CurrentPath = args[0];
+							Console.ReadLine();
+							break;
+					}
+					break;
+				default:
+					if (args.Length >= 2)
+					{
+						switch (args[0])
+						{
+							#region case:test
+							case "test":
+								PackageFile file = new PackageFile()
+								{
+									Actions = new PackFileAction[1] {
+								new PackFileAction() {
+									Type = "download",
+									Path ="test1/lol.txt",
+									Uri="http://tim.krals.ch" } }
+								};
+
+								PackageRegistryFile file2 = new PackageRegistryFile()
+								{
+									ServerName = "KingsoftMainNode",
+									Entrees = new PackageRegistryEntree[1] {
+								new PackageRegistryEntree() {
+									Id="test", Author="Tim Kral",
+									Description="it's just a test",
+									PackageFileUri="" } }
+								};
+
+								Console.WriteLine("");
+								Console.WriteLine("PFA");
+								Console.WriteLine(JsonConvert.SerializeObject(file, Formatting.Indented));
+								Console.WriteLine("");
+								Console.WriteLine("PRF");
+								Console.WriteLine(JsonConvert.SerializeObject(file2, Formatting.Indented));
+								return;
+							#endregion
+							case "install":
+								string path = Environment.CurrentDirectory;
+								string id = "";
+								for (int i = 1; i < args.Length; i++)
+								{
+									switch (args[i])
+									{
+										case "-id":
+											i++;
+											id = args[i];
+											break;
+										case "-p":
+											i++;
+											path = args[i];
+											break;
+										default:
+											break;
+									}
+								}
+								CurrentPath = path;
+								PackageManager.InstallPackageById(id);
+								return;
+							case "search":
+								string type = "";
+								string value = "";
+								for (int i = 1; i < args.Length; i++)
+								{
+									switch (args[i])
+									{
+										case "-id":
+											i++;
+											value = args[i];
+											type = "id";
+											break;
+										default:
+											break;
+									}
+								}
+								Table table = new Table().Centered();
+								table.AddColumn(new TableColumn("Id"));
+								table.AddColumn(new TableColumn("Author"));
+								table.AddColumn(new TableColumn("Package-File-Uri"));
+								table.AddColumn(new TableColumn("Description"));
+								table.Title = new TableTitle("Results");
+								AnsiConsole.Render(new Text("\n\n"));
+								AnsiConsole.Live(table).Start(ctx =>
+								{
+									ctx.Refresh();
+									bool PrintPackage(PackageRegistryEntree entree)
+									{
+										if (entree.Id == "<ERROR>")
+										{
+											table.AddRow(
+											new IRenderable[4] {
+										new Markup("0"),
+										new Markup("null"),
+										new Markup("null"),
+										new Markup("no result found")
+											});
+											ctx.Refresh();
+											return true;
+										}
+										table.AddRow(
+											new IRenderable[4] {
+										new Markup(entree.Id),
+										new Markup(entree.Author),
+										new Markup($"[u blue link={entree.PackageFileUri}]link[/]"),
+										new Markup(entree.Description)
+											});
+										ctx.Refresh();
+										return true;
+									}
+
+									PackageManager.SearchPackages(PrintPackage, value, type);
+									ctx.Refresh();
+								});
+
+								return;
+							default:
+								break;
+						}
+					}
+					break;
+			}
+		}
+	}
+}
